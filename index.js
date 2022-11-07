f3fec1c7db4f64c70f8f551ce2f0ff8c98df92a2
Asha Cole <maymunahrose@gmail.com>
2022-11-07 15:45:47 -0500

    fixed color-scheme attribute to data-color-scheme

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // THEME TOGGLE
 const themeToggle = document.querySelector(".circle");
 const doc = document.firstElementChild;
-const preferredTheme = localStorage.getItem("color-scheme")
-if(preferredTheme){
-    doc.setAttribute("color-scheme", preferredTheme)
-    themeToggle.setAttribute("data-theme", preferredTheme)
+const preferredTheme = localStorage.getItem("color-scheme");
+if (preferredTheme) {
+  doc.setAttribute("data-color-scheme", preferredTheme);
+  themeToggle.setAttribute("data-theme", preferredTheme);
 }
 themeToggle.addEventListener("click", () => {
   const currentTheme = themeToggle.getAttribute("data-theme");
   if (currentTheme === "blue") {
     themeToggle.setAttribute("data-theme", "white");
-    doc.setAttribute("color-scheme", "white");
-    localStorage.setItem("color-scheme", "white")
+    doc.setAttribute("data-color-scheme", "white");
+    localStorage.setItem("color-scheme", "white");
   } else if (currentTheme === "white") {
     themeToggle.setAttribute("data-theme", "purple");
-    doc.setAttribute("color-scheme", "purple");
-    localStorage.setItem("color-scheme", "purple")
+    doc.setAttribute("data-color-scheme", "purple");
+    localStorage.setItem("color-scheme", "purple");
   } else if (currentTheme === "purple") {
     themeToggle.setAttribute("data-theme", "blue");
-    doc.setAttribute("color-scheme", "blue");
-    localStorage.setItem("color-scheme", "blue")
+    doc.setAttribute("data-color-scheme", "blue");
+    localStorage.setItem("color-scheme", "blue");
   }
 });
 
@@ -46,35 +46,38 @@ const reset = document.querySelector("[data-action = 'reset']");
 const calculate = document.querySelector("[data-action = 'calculate']");
 
 const updateCalculation = (num) => {
-    if(display.textContent === "0"){
-        display.textContent = num 
-    } else{
-        display.textContent += num
-    }
-}
+  if (display.textContent === "0") {
+    display.textContent = num;
+  } else {
+    display.textContent += num;
+  }
+};
 const deleteValueFromCalculation = () => {
-    if(display.textContent.length === 1){display.textContent = "0"}else{display.textContent = display.textContent.slice(0, -1)}
-    
-}
-zero.addEventListener("click", ()=> updateCalculation(0))
-one.addEventListener("click", () => updateCalculation(1))
-two.addEventListener("click", () => updateCalculation(2))
-three.addEventListener("click", () => updateCalculation(3))
-four.addEventListener("click", () => updateCalculation(4))
-five.addEventListener("click", () => updateCalculation(5))
-six.addEventListener("click", () => updateCalculation(6))
-seven.addEventListener("click", () => updateCalculation(7))
-eight.addEventListener("click", () => updateCalculation(8))
-nine.addEventListener("click", () => updateCalculation(9))
-add.addEventListener("click", () => updateCalculation("+"))
-subtract.addEventListener("click", () => updateCalculation("-"))
-multiply.addEventListener("click", () => updateCalculation("*"))
-divide.addEventListener("click", () => updateCalculation("/"))
-decimal.addEventListener("click", () => updateCalculation("."))
-deleteValue.addEventListener("click", deleteValueFromCalculation)
+  if (display.textContent.length === 1) {
+    display.textContent = "0";
+  } else {
+    display.textContent = display.textContent.slice(0, -1);
+  }
+};
+zero.addEventListener("click", () => updateCalculation(0));
+one.addEventListener("click", () => updateCalculation(1));
+two.addEventListener("click", () => updateCalculation(2));
+three.addEventListener("click", () => updateCalculation(3));
+four.addEventListener("click", () => updateCalculation(4));
+five.addEventListener("click", () => updateCalculation(5));
+six.addEventListener("click", () => updateCalculation(6));
+seven.addEventListener("click", () => updateCalculation(7));
+eight.addEventListener("click", () => updateCalculation(8));
+nine.addEventListener("click", () => updateCalculation(9));
+add.addEventListener("click", () => updateCalculation("+"));
+subtract.addEventListener("click", () => updateCalculation("-"));
+multiply.addEventListener("click", () => updateCalculation("*"));
+divide.addEventListener("click", () => updateCalculation("/"));
+decimal.addEventListener("click", () => updateCalculation("."));
+deleteValue.addEventListener("click", deleteValueFromCalculation);
 reset.addEventListener("click", () => {
-    display.textContent = 0; 
-})
+  display.textContent = 0;
+});
 calculate.addEventListener("click", () => {
-    display.textContent = eval(display.textContent)
-})
+  display.textContent = eval(display.textContent);
+});
